8cfac513a94c6c630d99ce5393f4c8be5fa3f2fd
polo123456789 <pabsan02@gmail.com>
2022-12-08 21:07:23 -0600

    (+) Primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'idam-actividad6-21001135',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
